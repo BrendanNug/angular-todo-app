a568c93cb6aefc022860bb809b177fe7356153a9
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-04-08 11:03:23 -0400

    commit on 'master'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  //items: string[] = ["hi", "bye"];
+  items: object[];
+  todoCurrent: string;
+  constructor(){
+
+  }
+  ngOnInit(){
+    this.items = [];
+    this.todoCurrent = '';
+  }
+
   title = 'todo-app';
+
+  addItem(item){
+    this.items.push(item);
+  }
+
+  getItems(){
+    return this.items;
+  }
+  removeItem(item){
+    const index: number = this.items.indexOf(item);
+    if(index !== -1){
+      this.items.splice(index, 1);
+    }
+  }
+
+  
 }
+
+
+
